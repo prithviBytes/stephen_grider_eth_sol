8f3b5dc76ae47a13a488376717afc891ab881195
KSuvarna, Prithviraj - Dell Team <prithviraj_k_suvarna@dellteam.com>
2022-02-18 18:21:24 +0530

    add: tests

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,12 +2,42 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+const initialString = "Hello World!"
+let accounts = [];
+let inbox;
+beforeEach(async() => {
+    // Get List of all the Accounts
+    accounts = await web3.eth.getAccounts()
+
+    // Use one of the Accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [initialString] })
+    .send({ from: accounts[0], gas: "1000000" });
+})
 
 
+describe('Inbox', () => {
+    it('created accounts', () => {
+        assert.notEqual(accounts.length, 0)
+    });
 
+    it("deployed contract", () => {
+        assert.ok(inbox.options.address)
+    })
 
+    it('has default message', async () => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message, initialString)
+    })
 
-
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('Bye').send({ from: accounts[0] })
+        const message = await inbox.methods.message().call();
+        assert.equal( message, "Bye")
+    })
+});
 
 
 
