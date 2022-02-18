ed901aec047e7e176d4f7690b1f6f6e41edec817
KSuvarna, Prithviraj - Dell Team <prithviraj_k_suvarna@dellteam.com>
2022-02-18 16:30:15 +0530

    setup: tests

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,45 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Car {
+//     park() {
+//         return 'parked'
+//     }
+//     stop() {
+//         return 'stopped'
+//     }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//     car = new Car()
+// })
+
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'parked')
+//     })
+//     it('can stop', () => {
+//         assert.equal(car.stop(), 'stopped')
+//     })
+// })
