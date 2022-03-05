7b80d1d04c4c430456e497a5a169a3b059c449c7
prithviBytes <prithvisuvarna56@gmail.com>
2022-03-05 22:38:56 +0530

    add: web3 injection

--- lottery-react/src/web3.js
+++ lottery-react/src/web3.js
@@ -1,6 +1,3 @@
-### Method to access metamask
-
-```
 import Web3 from "web3";
  
 window.ethereum.request({ method: "eth_requestAccounts" });
@@ -8,4 +5,3 @@ window.ethereum.request({ method: "eth_requestAccounts" });
 const web3 = new Web3(window.ethereum);
  
 export default web3;
-```
