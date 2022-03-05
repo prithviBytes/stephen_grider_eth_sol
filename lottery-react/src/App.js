5df4561e003aa77df9f48ceb4da98d2013fec8a8
prithviBytes <prithvisuvarna56@gmail.com>
2022-03-05 23:25:27 +0530

    add: form

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,20 +1,42 @@
 import "./App.css";
 import React from "react";
 import lottery  from './lottery'
+import web3 from "./web3";
 class App extends React.Component {
   state = {
-    manager: ""
+    manager: "",
+    players: [],
+    balance: "",
+    value: ""
   };
 
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
   }
   render() {
     return (
       <div className="App">
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by: <b>{this.state.manager}</b></p>
+        <p>
+          This contract is managed by: <b>{this.state.manager}</b>.<br/>
+          There are currently {this.state.players.length} people entered in they current round.<br/>
+          Competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether.<br/>
+        </p>
+        <hr/>
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value})}
+            />
+            <button>Enter</button>
+          </div>
+        </form>
       </div>
     );
   }
