4a5670982cae3b086e81b21f932ea4abc8782627
prithviBytes <prithvisuvarna56@gmail.com>
2022-03-06 21:15:32 +0530

    final touch lottery-react

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -7,14 +7,61 @@ class App extends React.Component {
     manager: "",
     players: [],
     balance: "",
-    value: ""
+    value: "",
+    message: "",
+    isManager: false
   };
 
   async componentDidMount(){
+    await this.updateData();
+    const accounts = await web3.eth.getAccounts();
+    if ( this.state.manager === accounts[0] ){
+      this.setState({ isManager: true });
+    }
+  }
+
+  async updateData() {
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address);
     this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    if ( parseFloat(this.state.value) <= 0.1 ){
+      this.setState({
+        message: "Minimum entry amount is 0.1",
+        value: ""
+      });
+      return
+    }
+    this.setState({
+      message: "Waiting for trasaction to complete!!!"
+    })
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+    this.setState({
+      message: "Transaction Completed!!!"
+    })
+    this.updateData();
+  };
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({
+      message: "Waiting for transaction success"
+    })
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+    this.setState({
+      message: "Picked a Winner!!!"
+    })
   }
   render() {
     return (
@@ -26,17 +73,24 @@ class App extends React.Component {
           Competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether.<br/>
         </p>
         <hr/>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>
             <input 
               value={this.state.value}
+              placeholder="Amount greater than 0.1"
               onChange={event => this.setState({ value: event.target.value})}
             />
             <button>Enter</button>
           </div>
         </form>
+        <hr/>
+        {
+          this.state.isManager && <button onClick={this.onClick}>Pick Winner</button>
+        }
+        <hr/>
+        <h3>{this.state.message}</h3>
       </div>
     );
   }
